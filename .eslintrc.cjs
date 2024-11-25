49e536eac4daa99824c9e9efbd173682ee5466e9
XBanditxPandaX <108540082+XBanditxPandaX@users.noreply.github.com>
2024-11-25 12:41:10 +0100

    fix
    fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,7 @@ module.exports = {
     commonjs: true,
     es6: true,
   },
-  ignorePatterns: ["!**/.server", "!**/.client", "!**./site"],
+  ignorePatterns: ["!**/.server", "!**/.client", "site/"],
 
   // Base config
   extends: ["eslint:recommended"],
